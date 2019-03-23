965f5239a6a659f94782bd05f55086dd081b464f
BigBoiMotoMoto <calvin.barisic@mytools2go.ca>
2019-03-23 15:25:29 -0400

    Update game.py

--- game.py
+++ game.py
@@ -22,56 +22,75 @@ def levelselect():
         screen.blit(textsurface,(125,100))
         if mousex>=100 and mousex <=200 and mousey>=100 and mousey<=200 and mousedown:
             draw.rect(screen,(255,0,255),(0,0,100,100))
+            selected=True
         draw.rect(screen,(255,255,255),(250,100,100,100))
         textsurface = myfont.render('2',False,(0,0,0))
         screen.blit(textsurface,(275,100))
         if mousex>=250 and mousex <=350 and mousey>=100 and mousey<=200 and mousedown:
             draw.rect(screen,(255,0,255),(0,0,100,100))
+            selected=True
         draw.rect(screen,(255,255,255),(400,100,100,100))
         textsurface = myfont.render('3',False,(0,0,0))
         screen.blit(textsurface,(425,100))
         if mousex>=400 and mousex <=500 and mousey>=100 and mousey<=200 and mousedown:
             draw.rect(screen,(255,0,255),(0,0,100,100))
+            selected=True
 
         draw.rect(screen,(255,255,255),(100,250,100,100))
         textsurface = myfont.render('4',False,(0,0,0))
         screen.blit(textsurface,(125,250))
         if mousex>=100 and mousex <=200 and mousey>=250 and mousey<=350 and mousedown:
             draw.rect(screen,(255,0,255),(0,0,100,100))
+            selected=True
         draw.rect(screen,(255,255,255),(250,250,100,100))
         textsurface = myfont.render('5',False,(0,0,0))
         screen.blit(textsurface,(275,250))
         if mousex>=250 and mousex <=350 and mousey>=250 and mousey<=350 and mousedown:
             draw.rect(screen,(255,0,255),(0,0,100,100))
+            selected=True
         draw.rect(screen,(255,255,255),(400,250,100,100))
         textsurface = myfont.render('6',False,(0,0,0))
         screen.blit(textsurface,(425,250))
         if mousex>=400 and mousex <=500 and mousey>=250 and mousey<=350 and mousedown:
             draw.rect(screen,(255,0,255),(0,0,100,100))
+            selected=True
 
         draw.rect(screen,(255,255,255),(100,400,100,100))
         textsurface = myfont.render('7',False,(0,0,0))
         screen.blit(textsurface,(125,400))
         if mousex>=100 and mousex <=200 and mousey>=400 and mousey<=500 and mousedown:
             draw.rect(screen,(255,0,255),(0,0,100,100))
+            selected=True
         draw.rect(screen,(255,255,255),(250,400,100,100))
         textsurface = myfont.render('8',False,(0,0,0))
         screen.blit(textsurface,(275,400))
         if mousex>=250 and mousex <=350 and mousey>=400 and mousey<=500 and mousedown:
             draw.rect(screen,(255,0,255),(0,0,100,100))
+            selected=True
         draw.rect(screen,(255,255,255),(400,400,100,100))
         textsurface = myfont.render('9',False,(0,0,0))
         screen.blit(textsurface,(425,400))
         if mousex>=400 and mousex <=500 and mousey>=400 and mousey<=500 and mousedown:
             draw.rect(screen,(255,0,255),(0,0,100,100))
+            selected=True
+
+selected=False
 
 font.init()
 while running == True:
     for evt in event.get():
         if evt.type == QUIT:
             running = False
-    
+        elif evt.type == KEYDOWN:
+            if evt.key == K_l:
+                if selected:
+                    selected=False
+                else:
+                    selected=True
+    if selected:            
         levelselect()
+    else:
+        screen.fill((0,0,0))
 
     display.flip()
 quit()
