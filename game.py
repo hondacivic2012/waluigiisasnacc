28d308eb8f8315f949d8788c180b6696cca24d85
cabbagecabbagecabbage <48808428+cabbagecabbagecabbage@users.noreply.github.com>
2019-03-24 10:20:50 -0400

    Update game.py

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ from pygame import*
 import time
 import random
 
-print("Use arrow keys to control waluigi. Reach blue line to complete level.")
+print("Use arrow keys to control waluigi. Reach blue line to complete level. Collect poptarts for 100 points each! Score updates on next level.")
 
 screen=display.set_mode((600,600))
 display.set_caption('Select level, then press L to play a different level.')
@@ -12,20 +12,24 @@ imagelist = []
 running=True
 
 ##Loading Images##
-for i in range(1,8):  
+for i in range(1,10):  
     imagelist.append(transform.smoothscale(image.load("maze#"+str(i)+".png"), (600,600)))
 
 drawlist = []
-
+ 
+poptartx, poptarty= 0,0
 playerx, playery = 0,0
 previousx, previousy = playerx, playery
 size = 45
+updatescorecount = 0
 
 level = 0
 
 def levelselect():
-        global size,selected
-        global playerx, playery
+        global size,selected,speed,playerx, playery,poptartx,poptarty,pop,poptart, level, updatescorecount
+
+        pop=False
+        speed = 5
         screen.fill((200,0,200))
         myfont=font.SysFont('Comic Sans MS', 50)
         textsurface = myfont.render('Pick a Level',False,(225,255,255))
@@ -42,9 +46,11 @@ def levelselect():
         screen.blit(textsurface,(125,100))
         #first
         if mousex>=100 and mousex <=200 and mousey>=100 and mousey<=200 and mousedown:
+            updatescorecount = 0
             drawlist.append(imagelist[0])
             selected=True
             playerx, playery = 145, 470
+            poptartx,poptarty=40,475
             size = 100
             level = 1
         #################################    
@@ -53,8 +59,10 @@ def levelselect():
         screen.blit(textsurface,(275,100))
         #second
         if mousex>=250 and mousex <=350 and mousey>=100 and mousey<=200 and mousedown:
+            updatescorecount = 0
             size = 45
             playerx, playery = 28, 467
+            poptartx,poptarty=250,75
             drawlist.append(imagelist[1])
             selected=True
             level = 2
@@ -64,7 +72,9 @@ def levelselect():
         screen.blit(textsurface,(425,100))
         #third
         if mousex>=400 and mousex <=500 and mousey>=100 and mousey<=200 and mousedown:
+            updatescorecount = 0
             playerx, playery = 20,495
+            poptartx,poptarty=457,190
             drawlist.append(imagelist[2])
             size = 38
             selected=True
@@ -75,7 +85,9 @@ def levelselect():
         screen.blit(textsurface,(125,250))
         #fourth
         if mousex>=100 and mousex <=200 and mousey>=250 and mousey<=350 and mousedown:
+            updatescorecount = 0
             playerx, playery = 95,10
+            poptartx,poptarty=17,90
             drawlist.append(imagelist[3])
             size = 31
             selected=True
@@ -86,7 +98,9 @@ def levelselect():
         screen.blit(textsurface,(275,250))
         #fifth
         if mousex>=250 and mousex <=350 and mousey>=250 and mousey<=350 and mousedown:
+            updatescorecount = 0
             playerx, playery = 65, 550
+            poptartx,poptarty=215,285
             size = 28
             drawlist.append(imagelist[4])
             selected=True
@@ -97,7 +111,9 @@ def levelselect():
         screen.blit(textsurface,(425,250))
         #sixth
         if mousex>=400 and mousex <=500 and mousey>=250 and mousey<=350 and mousedown:
+            updatescorecount = 0
             playerx, playery = 25,565
+            poptartx,poptarty=168,280
             size = 20
             drawlist.append(imagelist[5])
             selected=True
@@ -108,8 +124,10 @@ def levelselect():
         screen.blit(textsurface,(125,400))
         #seventh
         if mousex>=100 and mousex <=200 and mousey>=400 and mousey<=500 and mousedown:
-            playerx, playery = 0,0
-            size = 16
+            updatescorecount = 0
+            playerx, playery = 305, 560
+            poptartx,poptarty=501,360
+            size = 24
             drawlist.append(imagelist[6])
             selected=True
             level = 7
@@ -119,7 +137,9 @@ def levelselect():
         screen.blit(textsurface,(275,400))
         #eighth
         if mousex>=250 and mousex <=350 and mousey>=400 and mousey<=500 and mousedown:
+            updatescorecount = 0
             playerx, playery = 565, 265
+            poptartx,poptarty=548,361
             size = 16
             drawlist.append(imagelist[7])
             selected=True
@@ -130,20 +150,29 @@ def levelselect():
         screen.blit(textsurface,(425,400))
         #nineth
         if mousex>=400 and mousex <=500 and mousey>=400 and mousey<=500 and mousedown:
-            drawlist.append(map9)
+            updatescorecount = 0
+            drawlist.append(imagelist[8])
+            playerx, playery = 7, 100
+            poptartx,poptarty=592,500
+            size = 6
+            speed = 2
             selected=True
             level = 9
-        #################################  
+        #################################
+        poptart = transform.smoothscale(image.load("poptarts.png"), (size, size))
 selected=False
-
+pop = False
+collided = set([])
+point = 0
 #print([(j,0,i,n)for j in range(0,6) for i in range(248,256) for n in range(248,256)])
-speed = 5
 l,r,d,u = 0,0,0,0
 movement = False
 
 font.init()
 while running == True:
     waluigi = transform.smoothscale(image.load("waluigi.jpg"), (size,size))
+    playerrect = Rect(playerx, playery, size, size)
+    poprect = Rect(poptartx, poptarty, size, size)
     screen.fill((0,0,0))
     for evt in event.get():
         if evt.type == QUIT:
@@ -160,10 +189,22 @@ while running == True:
     #Player Interface                
     if selected==False:          
         levelselect()
+        if updatescorecount == 0:
+            print(len(collided)*100)
+            updatescorecount += 1
     else:
         if len(drawlist) != 0:
             screen.blit(drawlist[-1],(0,0))
         screen.blit(waluigi,(playerx,playery))
+        #draw.rect(screen, (255,255,255), poprect)
+        if playerrect.colliderect(poprect):
+            pop = True
+            collided.add(level)
+            
+        if pop==False:
+            screen.blit(poptart,(poptartx,poptarty))
+
+            
         
     
     
@@ -231,4 +272,5 @@ while running == True:
         movement = True
     
     display.flip()
+print(point)
 quit()
